eabe7b01a362bb574522bea25db3f2c090e9ce63
William Meyer <Valodier@hotmail.com>
2022-12-07 17:38:02 -0500

    Adds Unit Tests for PerformWork functionality

--- BethenysPieShopHRM.Tests/UnitTest1.cs
+++ BethenysPieShopHRM.Tests/UnitTest1.cs
@@ -10,11 +10,27 @@ namespace BethenysPieShopHRM.Tests
         {
             //Arrange
             Employee employee = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
-            
+
+            int numberOfHours = 3;
 
             //Act
+            employee.PerformWork(numberOfHours);
 
             //Assert
+            Assert.Equal(numberOfHours, employee.NumberOfHoursWorked);
+        }
+
+        [Fact]
+        public void PerformWork_Adds_DefaultNumberOfHours_IfNoValueSpecified()
+        {
+            //Arrange
+            Employee employee = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+            //Act
+            employee.PerformWork();
+
+            //Assert
+            Assert.Equal(1, employee.NumberOfHoursWorked);
         }
     }
 }
